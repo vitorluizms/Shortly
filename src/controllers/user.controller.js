4cb825267dbade1a450822b2f0c0f6dbfcea81d6
vitorluizms <vitor.luiz.eer@gmail.com>
2023-08-03 11:19:24 -0300

    feat: Rota de login criada

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import db from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
   const { name, email, password, confirmPassword } = req.body;
@@ -21,4 +22,27 @@ export async function signUp(req, res) {
   }
 }
 
-export async function signIn(req, res) {}
+export async function signIn(req, res) {
+  const { email, password } = req.body;
+  try {
+    const userValid = await db.query(`SELECT * FROM users WHERE email = $1`, [
+      email,
+    ]);
+    console.log(userValid.rows);
+    if (
+      userValid.rowCount === 0 ||
+      !bcrypt.compareSync(password, userValid.rows[0].password)
+    )
+      return res.status(401).send(userValid.rows);
+
+    const token = uuid();
+    await db.query(`DELETE FROM sessions WHERE "userId" = $1`, [userValid.rows[0].id])
+    await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [
+      userValid.rows[0].id,
+      token,
+    ]);
+    res.status(200).send({ token: token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
