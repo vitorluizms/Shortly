ec3b14140de49cf9d4f48189d99d030368766e31
vitorluizms <vitor.luiz.eer@gmail.com>
2023-08-04 11:28:58 -0300

    feat: Rota de busca de todas as URL do usuário adicionada

--- src/controllers/rank.controller.js
+++ src/controllers/rank.controller.js
@@ -1,7 +1,26 @@
 import db from "../database/database.connection.js";
 
-export async function getAllUrlsByUser(req, res){
+export async function getAllUrlsByUser(req, res) {
+  const { user } = res.locals;
+  try {
+    const promise = await db.query(
+      `SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount",
+      JSON_AGG(JSON_BUILD_OBJECT(
+        'id', urls.id,
+        'shortUrl', urls."shortUrl",
+        'url', urls.url,
+        'visitCount', urls."visitCount"
+      ) ORDER BY urls.id) AS shortenedUrls
+      FROM users
+      JOIN urls ON urls."userId" = users.id
+      WHERE users.id = $1
+      GROUP BY users.id`,
+      [user.userId]
+    );
+    res.send(promise.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
-export async function getUrlsRank(req, res){
-}
+export async function getUrlsRank(req, res) {}
